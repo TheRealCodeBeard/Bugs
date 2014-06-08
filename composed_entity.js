b3fa61284e514f74e70c3e321138d6ebc73d162f
Phil Harvey <phil@datashaka.com>
2014-06-08 08:21:20 +0100

    split out composed_entity

---
+++ composed_entity.js
@@ -0,0 +1,35 @@
+var ComposedEntity = function(x,y,parts){
+	var me = this;
+	me.id = support.getID();
+	me.x = x;
+	me.y = y;
+	me.rotation = 0;
+	me.size = 7;
+	me.parts = parts;
+};
+
+ComposedEntity.prototype.getGroup = function(){
+	var me = this;
+	var idloc = {x:-me.size-2,y:-me.size-2};
+
+	me.group = drawing.getAGroup();
+	me.group.appendChild(drawing.getCircle(0,0,me.size,"white"));
+	me.group.appendChild(drawing.getText(idloc.x,idloc.y,me.id,"gray"));
+
+	var addPart = function(p) {me.group.appendChild(me.parts[p].physical(me))};
+	for(var i=0;i<me.parts.length;i++){addPart(i);}
+
+	me.update();
+	return me.group;
+};
+
+ComposedEntity.prototype.update = function(){
+	var me = this;
+	me.group.setAttribute("transform","translate("+me.x+","+me.y+"),rotate("+me.rotation+")");
+};
+
+ComposedEntity.prototype.tick = function(){
+	var me = this;
+	me.parts.performOn("act",me);
+	me.update();
+};
